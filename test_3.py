e2af59d68ee13bab26b5b673f454bb5978b30083
IhorVitko <igor.vitko.74@gmail.com>
2021-07-20 08:59:17 +0300

    final commit master

--- test_3.py
+++ test_3.py
@@ -11,4 +11,3 @@ res_path = os.path.join(main_path, "resources")
 with open(os.path.join(res_path, "data.json")) as f:
     data_resource = json.loads(f.read()).get("place")
     print(data_resource)
-# print(res_path)
