4d585e9d28b0601ce128c79b7ee7e0b003fd0007
IhorVitko <igor.vitko.74@gmail.com>
2021-07-20 08:38:50 +0300

    add os path

--- test_3.py
+++ test_3.py
@@ -1,4 +1,10 @@
 import random
+import os
 
 print(random.randint(2, 8))
 print(random.randint(10, 50))
+
+main_path = os.path.dirname(os.path.abspath(__file__))
+res_path = os.path.join(main_path, "resources")
+
+print(res_path)
