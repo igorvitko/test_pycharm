6ff5d14796e33937ca9961f24348d251fe45c701
IhorVitko <igor.vitko.74@gmail.com>
2021-07-20 08:54:09 +0300

    add json file

--- test_3.py
+++ test_3.py
@@ -1,3 +1,4 @@
+import json
 import random
 import os
 
@@ -7,4 +8,7 @@ print(random.randint(10, 50))
 main_path = os.path.dirname(os.path.abspath(__file__))
 res_path = os.path.join(main_path, "resources")
 
-print(res_path)
+with open(os.path.join(res_path, "data.json")) as f:
+    data_resource = json.loads(f.read()).get("place")
+    print(data_resource)
+# print(res_path)
