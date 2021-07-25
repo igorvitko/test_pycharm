c8d47b3a5d725b4757ed2d5095105ce288a7f576
IhorVitko <igor.vitko.74@gmail.com>
2021-07-25 11:37:18 +0300

    add new functonality app.py

--- app.py
+++ app.py
@@ -1,10 +1,19 @@
+import datetime
+import json
+import os
+
 from flask import Flask
+
+PATH_MAIN = os.path.dirname(os.path.abspath(__file__))
+RES_PATH = os.path.join(PATH_MAIN, "resources")
 
 app = Flask(__name__)
 
 @app.route('/')
 def start():
-    return "Hello, world!"
+    with open(os.path.join(RES_PATH, "data.json")) as f:
+        data_resource = json.loads(f.read()).get("place")
+    return f"{data_resource} - {datetime.datetime.now().strftime('%d.%m.%Y %H:%M:%S')}"
 
 
 if __name__ == "__main__":
